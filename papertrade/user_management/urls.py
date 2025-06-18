6dae9d2c99c1493868ca64e0be27d24e0f48aaa2
Eurie-R <ramoy.uriel@gmail.com>
2025-06-18 23:27:51 +0800

    Started the base of the user management
    Added
    -Templates folder for registration
    -Profile homepage
    -Profile model
    -initialized Base.html and homepage.html
    -added DIRS os path in settings.py

---
+++ papertrade/user_management/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+from . import views
+
+
+urlpatterns = [
+    path('', views.homepage, name="homepage")
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+app_name = 'user_management'
