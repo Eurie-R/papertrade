ea344cd26a8cb8473fdabc7590b46bfcd71dde27
Eurie-R <ramoy.uriel@gmail.com>
2025-06-21 22:30:53 +0800

    Created the alpaca_client file

---
+++ papertrade/tradeapp/alpaca_client.py
@@ -0,0 +1,32 @@
+ 
+from alpaca.trading.client import TradingClient
+from alpaca.data.historical import StockHistoricalDataClient
+from alpaca.data.requests import CryptoLatestQuoteRequest 
+from alpaca.data.timeframe import TimeFrame
+import sys
+import os
+
+
+# Get the absolute path to the root directory 
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Add it to sys.path if not already added
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
+
+# Now you can safely import creds
+try:
+    import creds
+except ImportError:
+    raise ImportError("Could not find creds.py. Ensure it's in the project root and not renamed.")
+
+API_KEY = creds.api_key
+SECRET_KEY = creds.api_secret_key
+BASE_URL = creds.api_base_URL
+
+#Paper trading client 
+trading_client = TradingClient(API_KEY, SECRET_KEY, paper=True)
+
+#Market Data 
+data_client = StockHistoricalDataClient(API_KEY, SECRET_KEY)
+
